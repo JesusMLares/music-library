9bf59659037cde797616c69dfcd87632d9768681
JesusMLares <twinlares2@hotmail.com>
2024-06-12 18:52:01 -0700

    withContext branch finished :wq

---
+++ src/components/Gallery.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import GalleryItem from './GalleryItem';
+
+function Gallery(props){
+    const display = props.data.map((item, index) => {
+        return(
+            <GalleryItem key={index} item={item} />
+        )
+    })
+
+    return(
+        <div>
+            {display}
+        </div>
+    )
+}
+
+export default Gallery;
